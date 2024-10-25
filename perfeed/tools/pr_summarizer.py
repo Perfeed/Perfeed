b2c05e5c7a44f58e011a4c36444e2e3cbd4dcb05
Ang Wang <ang@perfeed.io>
2024-10-25 10:57:28 +0800

    refactor PRSummarizer to use BaseGitProvider and change to async run

--- perfeed/tools/pr_summarizer.py
+++ perfeed/tools/pr_summarizer.py
@@ -4,19 +4,20 @@ import requests
 from jinja2 import Environment, StrictUndefined
 
 from perfeed.config_loader import settings
-from perfeed.git_providers import github
+from perfeed.git_providers.github import GithubProvider
+from perfeed.git_providers.base import BaseGitProvider
 from perfeed.llms.base_client import BaseClient
 from perfeed.llms.ollama_client import OllamaClient
 from perfeed.models.pr_summary import PRSummary
 from perfeed.utils import json_output_curator
 
 class PRSummarizer:
-    def __init__(self, owner: str, llm: BaseClient):
-        self.git = github.GithubProvider(owner=owner)
+    def __init__(self, git: BaseGitProvider, llm: BaseClient):
+        self.git = git
         self.llm = llm
 
-    def run(self, repo: str, pr_number: int) -> PRSummary:
-        pr = asyncio.run(self.git.get_pr(repo, pr_number))
+    async def run(self, repo: str, pr_number: int) -> PRSummary:
+        pr = await self.git.get_pr(repo, pr_number)
 
         self.variables = {
             "author": pr.author,
@@ -42,6 +43,6 @@ class PRSummarizer:
 
 
 if __name__ == "__main__":
-    summarizer = PRSummarizer(owner="Perfeed", llm=OllamaClient("llama3.2"))
+    summarizer = PRSummarizer(GithubProvider("Perfeed"), llm=OllamaClient("llama3.2"))
     pr_summary = summarizer.run("perfeed", 5)
     print(pr_summary)
